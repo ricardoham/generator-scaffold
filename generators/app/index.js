c68ec55193b9e7064df0679ff8c33a866bdf4ef5
Ricardo Manoel Junior <rmanoel@brdlrmanoel.daitan.com>
2019-02-13 14:30:15 -0200

    initial setup

---
+++ generators/app/index.js
@@ -0,0 +1,12 @@
+var Generator = require('yeoman-generator');
+var chalk
+
+module.exports = class extends Generator {
+  method1() {
+    this.log('method 1 just ran');
+  }
+
+  method2() {
+    this.log('method 2 just ran');
+  }
+};
